752c5b6d3203ccadb87d1e379054e07d85930384
1RONAN1 <rdfgaming2019@gmail.com>
2024-02-19 20:43:58 +0530

    Updater: adapt updater for FireDroid
    Change-Id: I4df073417ae96dce2fd1f507d2162f402f0f22f6

---
+++ app/src/main/java/net/firedroid/ota/model/MaintainerInfo.java
@@ -0,0 +1,14 @@
+package net.firedroid.ota.model;
+
+public class MaintainerInfo {
+    private String name;
+    private String github_username;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return github_username;
+    }
+}
